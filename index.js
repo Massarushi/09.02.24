75887399d8804633baed8e7dd9e17280a47070a6
Massarushi <142435857+Massarushi@users.noreply.github.com>
2024-02-09 10:54:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+// console.log("Hello World");
+
+var x = 1;
+var y = 2;
+
+if ( x == y) {
+      console.log("Números conferem!");
+} else {
+    console.log("Números divergem!");
+}
+
